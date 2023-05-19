8a81e1d6541dd42f5ec9c7ac91fba8bc715ceb23
Edward Bera <47110064+edwardbera@users.noreply.github.com>
2023-05-19 12:44:25 +0200

    Update MainView.js

--- src/Views/MainView.js
+++ src/Views/MainView.js
@@ -15,7 +15,7 @@ export default function ContentArea( props){
     
     //Prop setting the current playing tract
     props.selUrl(current);
-    console.log(uri)
+    console.log("url is :", uri)
     //Function to set the current playing tract
     const curl = (current) => {
         setCurrent(current);
